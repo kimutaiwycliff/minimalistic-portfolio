2a58e145dce204fc6add9646eddbd99692321024
Wycliff Kimutai <kcliff@tutanota.com>
2024-09-06 13:46:48 +0300

    scaffolding the project

---
+++ app/(home)/components/NavBar.tsx
@@ -0,0 +1,8 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <h1 className=""> Wycliff Kimutai 🥷🏽</h1>
+    </nav>
+  );
+}
+export default NavBar
---
+++ app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import NavBar from "./components/NavBar"
+
+const HomePage = () => {
+  return (
+    <div className="min-h-screen bg-black">
+      <div className="max-w-7xl mx-auto">
+        <NavBar />
+      </div>
+    </div>
+  )
+}
+export default HomePage
