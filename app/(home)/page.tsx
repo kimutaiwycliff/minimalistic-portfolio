5b34caba7678cfd9b11b8904e0f588f2253f9c07
Wycliff Kimutai <kcliff@tutanota.com>
2024-09-07 16:27:40 +0300

    tailwind config for acertinity

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,9 +4,11 @@ import NavBar from "./components/NavBar"
 const HomePage = () => {
   return (
     <div className="min-h-screen bg-black overflow-hidden">
+      <div className="dark:bg-black bg-white  dark:bg-grid-white/[0.07] bg-grid-black/[0.2]">
       <div className="max-w-7xl mx-auto p-5">
         <NavBar />
         <HeroSection />
+      </div>
       </div>
     </div>
   )
