d761c04df8859a5564954b1ef8ededcda7f59196
Wycliff Kimutai <kcliff@tutanota.com>
2024-09-07 07:15:50 +0300

    feat(navbar):social links and name

--- app/(home)/components/NavBar.tsx
+++ app/(home)/components/NavBar.tsx
@@ -1,8 +1,21 @@
+import { socials } from '@/utils/data';
+import Link from 'next/link';
+
 const NavBar = () => {
   return (
-    <nav>
-      <h1 className=""> Wycliff Kimutai 🥷🏽</h1>
+    <nav className="py-10 flex justify-between items-center">
+      <h1 className="text-2xl font-bold underline underline-offset-8 decoration-green-400 -rotate-2">
+        {' '}
+        Wycliff Kimutai 🥷🏽
+      </h1>
+      <div className="flex items-center gap-5">
+        {socials.map((social, index) => (
+          <Link href={social.link} key={index} aria-label={social.label}>
+            <social.Icon className='w-5 h-5 hover:scale-125 transition-all'/>
+          </Link>
+        ))}
+      </div>
     </nav>
   );
-}
-export default NavBar
+};
+export default NavBar;
---
+++ utils/data.ts
@@ -0,0 +1,18 @@
+import { SiGithub,SiLinkedin, SiX } from "react-icons/si";
+export const socials = [
+    {
+        link: "https://www.linkedin.com/in/wycliff-kimutai/",
+        label: "LinkedIn",
+        Icon: SiLinkedin
+    },
+    {
+        link: "https://www.linkedin.com/in/wycliff-kimutai/",
+        label: "GitHub",
+        Icon: SiGithub
+    },
+    {
+        link: "https://www.linkedin.com/in/wycliff-kimutai/",
+        label: "Twitter",
+        Icon: SiX
+    },
+]
