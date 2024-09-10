646972a1421cd6a5702074982108e5b29d10a17a
Wycliff Kimutai <kcliff@tutanota.com>
2024-09-10 11:06:54 +0300

    production ready fix

--- components/ui/card-hover-effect.tsx
+++ components/ui/card-hover-effect.tsx
@@ -1,6 +1,5 @@
 import { cn } from '@/lib/utils';
 import { AnimatePresence, motion } from 'framer-motion';
-import Link from 'next/link';
 import { useState } from 'react';
 import { IconType } from 'react-icons';
 
@@ -14,7 +13,7 @@ export const HoverEffect = ({
   }[];
   className?: string;
 }) => {
-  let [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
   return (
     <div
