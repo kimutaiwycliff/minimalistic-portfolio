8a7d263b6fb24ac542785d7a2fbba7419ec73940
Wycliff Kimutai <kcliff@tutanota.com>
2024-09-10 11:34:54 +0300

    animations

--- app/(home)/components/NavBar.tsx
+++ app/(home)/components/NavBar.tsx
@@ -4,7 +4,12 @@ import Link from 'next/link';
 
 const NavBar = ({ className }: { className?: string }) => {
   return (
-    <nav className={cn('py-10 flex justify-between items-center', className)}>
+    <nav
+      className={cn(
+        'py-10 flex justify-between items-center animate-move-down',
+        className
+      )}
+    >
       <h1 className="text-2xl font-bold underline underline-offset-8 decoration-green-400 -rotate-2">
         {' '}
         Wycliff Kimutai 🥷🏽
